e2c0d176e64b0d8631d46c04fc520b62d2491aa2
Himanshu Singh <shimanshu.3107@gmail.com>
2025-04-05 01:45:47 +0530

    Updates

--- frontend/vite.config.js
+++
@@ -1,8 +0,0 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  base: '/Virtual-AI_Assistant/', // Replace with your GitHub repo name
-  plugins: [react()],
-})
